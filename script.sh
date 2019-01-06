20370ac8d296f76d9d22323c276dcc66de3b1aa7
Anastasia Pyltsova <aspyltsova.fitu16@kubg.edu.ua>
2019-01-06 21:57:16 +0200

    ex01 almost there

--- script.sh
+++ script.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-parameter=$1
-if [[ $(which python) == "/usr/bin/python" ]]
-then 
+# parameter=$1
+# if [[ $(which python) == "/usr/bin/python" ]]
+# then 
 	# $(mv python_env/ft_sommelier.ipynb python_env/resources ../)
-	echo $(rm -rf $parameter) $(python3 -m venv $parameter)
+	# echo $(rm -rf $parameter) $(python3 -m venv $parameter)
 	# $(sleep 3s)
 	source python_env/bin/activate
-	echo "ny dopystim"
+	cd python_env 
+	jupyter notebook ft_sommelier.ipynb 
+	echo "~~~fse poka~~~~"
 	# $(mv ft_sommelier.ipynb resources $parameter)
 	# $(cd parameter) $(pip install matplotlib jupyter pandas)
 	# $(jupyter notebook ft_sommelier.ipynb)
-else
-	echo "NO"
-fi
+# else
+# 	echo "NO"
+# fi
